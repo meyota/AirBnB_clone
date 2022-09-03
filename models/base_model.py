72cf55caf053942e72c88b96bb40e0c6edcd5175
meyota <meyota08@gmail.com>
2022-09-03 11:03:26 -0700

    proj

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ import uuid
 class BaseModel:
     """Defines all common attributes/methods for other classes."""
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         # public instance attributes
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
