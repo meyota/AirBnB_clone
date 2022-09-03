7d6a4f7f55fad343884ed74b34309b20c0e60332
meyota <meyota08@gmail.com>
2022-09-03 09:26:17 -0700

    proj

--- models/base_model.py
+++ models/base_model.py
@@ -8,21 +8,12 @@ import uuid
 class BaseModel:
     """Defines all common attributes/methods for other classes."""
 
-    def __init__(self, *args, **kwargs):
-        """Initialize BaseModel class"""
-
+    def __init__(self):
+        """initialize BaseModel class"""
         # public instance attributes
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
-        if kwargs:
-            for key in kwargs.keys():
-                if key != "__class__":
-                    if key not in ["created_at", "updated_at"]:
-                        self.__setattr__(key, kwargs[key])
-                    else:
-                        self.__setattr__(key, datetime.fromisoformat(kwargs[key]))
 
     # public instance methods
     def __str__(self):
