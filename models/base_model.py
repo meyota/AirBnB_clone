68d067534d331b51c6cd6f19e799677103fa7dfd
meyota <meyota08@gmail.com>
2022-09-03 11:12:11 -0700

    proj

--- models/base_model.py
+++ models/base_model.py
@@ -9,10 +9,18 @@ class BaseModel:
     """Defines all common attributes/methods for other classes."""
 
     def __init__(self, **kwargs):
-        # public instance attributes
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key in kwargs.keys():
+                if key != "__class__":
+                    if key not in ["created_at", "updated_at"]:
+                        self.__setattr__(key, kwargs[key])
+                    else:
+                        self.__setattr__(key, datetime.fromisoformat(kwargs[key]))
+        else:
+            # public instance attributes
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     # public instance methods
     def __str__(self):
