515a9cfa93eb8b7536af6b57430e895b7dedc54a
meyota <meyota08@gmail.com>
2022-09-03 11:30:52 -0700

    proj

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!usr/bin/python3
 """BaseModel class."""
+
 import models
 from datetime import datetime
 import uuid
@@ -8,7 +9,14 @@ import uuid
 class BaseModel:
     """Defines all common attributes/methods for other classes."""
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
+        """Initialize BaseModel class"""
+
+        # public instance attributes
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
         if kwargs:
             for key in kwargs.keys():
                 if key != "__class__":
@@ -16,11 +24,6 @@ class BaseModel:
                         self.__setattr__(key, kwargs[key])
                     else:
                         self.__setattr__(key, datetime.fromisoformat(kwargs[key]))
-        else:
-            # public instance attributes
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
 
     # public instance methods
     def __str__(self):
@@ -35,6 +38,6 @@ class BaseModel:
         """returns a dictionary containing all keys/values"""
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = self.__class__.__name__
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        my_dict["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         return my_dict
